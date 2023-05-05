8bd588a6c76d6107c5292b03c10de7a6c23415f1
loretari <loretari@gmail.com>
2023-05-05 16:07:24 +0300

    init: add loader() Code in EventsPage

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -11,3 +11,14 @@ const events = useLoaderData();
 }
 
 export default EventsPage;
+
+export async function loader () {
+    const response = await fetch('http://localhost:8080/events');
+
+    if (!response.ok) {
+        //...
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
+}
