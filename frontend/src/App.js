5a331961c00df29c37b8e8786f42219b014b31c7
loretari <loretari@gmail.com>
2023-05-05 19:06:43 +0300

    init: add Extracting Error Data & throwing Responses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,9 +28,13 @@ import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
-    {path: '/', element: <RootLayout/>, children: [
+    {path: '/',
+        element: <RootLayout/>,
+        errorElement: <ErrorPage/>,
+        children: [
             {index: true, element: <HomePage/>},
             {path: 'events', element: <EventsRootLayout/>, children: [
                     {index: true, element: <EventsPage/>,
