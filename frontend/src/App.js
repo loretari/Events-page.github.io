ecd9fc0ee9a27c60446861e7b3a426416f217809
loretari <loretari@gmail.com>
2023-05-08 20:27:12 +0300

    init: working with action() Functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ import { RouterProvider, createBrowserRouter} from 'react-router-dom';
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader} from "./pages/Events";
 import EventDetailPage, {loader as eventDetailLoader} from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, {action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -53,7 +53,7 @@ const router = createBrowserRouter([
                     },
 
 
-                    {path: 'new', element: <NewEventPage/>},
+                    {path: 'new', element: <NewEventPage/>, action: newEventAction },
 
                 ]},
 
