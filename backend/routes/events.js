70aa79eaa9a4ead9c880553c412380fdc732ee6b
loretari <loretari@gmail.com>
2023-05-10 15:12:27 +0300

    init: deferring Data Fetching with defer()

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -12,8 +12,10 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     try {
         const events = await getAll();
+setTimeout(() => {
+    res.json({ events: events });
+}, 2000);
 
-            res.json({ events: events });
 
         } catch (error) {
             next(error);
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,29 +1,35 @@
-import { useLoaderData, json} from 'react-router-dom';
+import { useLoaderData, json, defer, Await} from 'react-router-dom';
+import { Suspense} from 'react';
 
 import EventsList from '../components/EventsList';
 
+
 function EventsPage() {
-const data = useLoaderData();
-// if (data.isError) {
-//     return <p>{data.message}</p>
-// }
+const {events} = useLoaderData();
 
-const events = data.events;
 
-    return <EventsList events={events} />
+    return (
+        <Suspense fallback = {<p style={{textAlign: 'center'}}>Loading...</p>}>
+            <Await resolve= {events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+
+        )
+
 
 
 }
 
 export default EventsPage;
 
-export async function loader () {
+async function loadEvents() {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
-      // return { isError: true, message: 'Could not fetch events.'};
-      //   throw new Response(JSON.stringify( { message: 'Could not fetch events.'}), {status: 500,});
-       throw json(
+        // return { isError: true, message: 'Could not fetch events.'};
+        //   throw new Response(JSON.stringify( { message: 'Could not fetch events.'}), {status: 500,});
+        throw json(
             { message: 'Could not fetch events.'},
             {
                 status: 500,
@@ -31,6 +37,13 @@ export async function loader () {
         )
     } else {
 
-        return response;
+        const resData = await response.json();
+            return resData.events;
     }
+}
+
+export function loader () {
+return defer({
+    events: loadEvents()
+})
 }
