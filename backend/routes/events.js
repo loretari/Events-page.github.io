fd009e472e06380a71d95046eaa1514a70db80cb
loretari <loretari@gmail.com>
2023-05-11 16:22:34 +0300

    init: add Authentication Works

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const { getAll, get, add, replace, remove } = require('../data/event');
+const { checkAuth } = require('../util/auth');
 const {
     isValidText,
     isValidDate,
@@ -10,18 +11,13 @@ const {
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
+    console.log(req.token);
     try {
         const events = await getAll();
-setTimeout(() => {
-    res.json({ events: events });
-}, 2000);
-
-
-        } catch (error) {
-            next(error);
-        }
-
-
+        res.json({ events: events });
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get('/:id', async (req, res, next) => {
@@ -33,7 +29,10 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
+router.use(checkAuth);
+
 router.post('/', async (req, res, next) => {
+    console.log(req.token);
     const data = req.body;
 
     let errors = {};
